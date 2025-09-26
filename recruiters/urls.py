750c9140b479bab1909ecb8791c161df009ddcfc
Brandon Marshall <brandonmarshalljr07@gmail.com>
2025-09-26 16:54:48 -0400

    Added view for candidates as recruiter (no search)

--- recruiters/urls.py
+++ recruiters/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.index, name='recruiters.index'),
     path('create/', views.create, name='recruiters.create'),
     #path('applications/', views.applications, name='posts.applications'),
+    path('candidates', views.candidates, name='recruiters.candidates'),
     path('<int:id>/post_id/', views.detail, name='recruiters.detail'),
     path('<int:id>/post_id/edit/', views.edit, name='recruiters.edit'),
     path('<int:id>/post_id/delete/', views.delete, name='recruiters.delete'),
